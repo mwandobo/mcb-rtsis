409745f73cd7b15fca895a73af1edddc88864fa9
shauritanga <curtisisaac36@gmail.com>
2025-12-31 14:47:37 +0300

    added atm and card transaction

---
+++ sqls/atm_transaction.sql
@@ -0,0 +1,20 @@
+select
+    CURRENT_TIMESTAMP as reportingDate,
+    atx.TERMINAL as atmCode,
+    atx.TUN_DATE as transactionDate,
+    atx.REFERENCE_NUMBER as transactionId,
+    CASE
+        WHEN atx.PROCESSING_CODE IN ('001000','002000','011000','011096','012000') THEN 'Cash Withdrawal'
+        WHEN atx.PROCESSING_CODE = '219610' THEN 'Purchase / Payment'
+        WHEN atx.PROCESSING_CODE IN ('311000','312000','381000','382000') THEN 'Non-Financial'
+        ELSE 'Others'
+    END as transactionNature,
+    'TZS' as currency,
+    atx.TRANSACTION_AMOUNT as orgTransactionAmount,
+    atx.TRANSACTION_AMOUNT as tzsTransactionAmount,
+    'Card and Mobile Based' as atmChannel,
+   DECIMAL(atx.TRANSACTION_AMOUNT * 0.18, 15, 2) AS valueAddedTaxAmount,
+    0 as exciseDutyAmount,
+    0 as electronicLevyAmount
+FROM ATM_TRX_RECORDING atx
+LEFT JOIN ATM_PROCESS_CODE pc ON pc.ISO_CODE = atx.PROCESSING_CODE WHERE TERMINAL in('MWL01001','MWL01002');
