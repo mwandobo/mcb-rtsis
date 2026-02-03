dbb1f37a67a148a331b6c71da4d9215f953bdc35
boniface mwandobo <bonifacesimon04@gmail.com>
2026-02-03 11:02:25 +0300

    customer type lookup

--- sqls/CREATE-CUSTOMER-TYPE-LOOKUP.sql
+++ sqls/CREATE-CUSTOMER-TYPE-LOOKUP.sql
@@ -2,7 +2,7 @@
 create table CUSTOMER_TYPES_LOOKUP
 (
     CUSTOMER_TYPE_CODE    CHAR(20)  not  null,
-    CUST_TYPE     CHAR(100)     null,
+    CUSTOMER_TYPE     CHAR(100)     null,
     CREATED_AT  CHAR(20)   not  null,
 
     constraint CUSTOMER_TYPES_LOOKUP_PK
