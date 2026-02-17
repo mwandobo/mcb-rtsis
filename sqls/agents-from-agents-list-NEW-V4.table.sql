eb5b9249d18a071dd75f86788158670b4652487b
boniface mwandobo <bonifacesimon04@gmail.com>
2026-02-17 10:49:42 +0300

    agents information refactor

--- sqls/agents-from-agents-list-NEW-V4.table.sql
+++ sqls/agents-from-agents-list-NEW-V4.table.sql
@@ -1,9 +1,8 @@
 SELECT VARCHAR_FORMAT(CURRENT_TIMESTAMP, 'DDMMYYYYHHMM') AS reportingDate,
        al.AGENT_NAME                                     AS agentName,
-       al.TERMINAL_ID                                    AS TerminalID,
        al.AGENT_ID                                       AS agentId,
        al.TILL_NUMBER                                    AS tillNumber,
-       al.BUSINESS_FORM                                  AS businessForm,
+       COALESCE(al.BUSINESS_FORM, 'Sole Proprietor')     AS businessForm,
        al.AGENT_PRINCIPAL                                AS agentPrincipal,
        al.AGENT_PRINCIPAL_NAME                           AS agentPrincipalName,
        al.GENDER                                         AS gender,
@@ -11,7 +10,11 @@ SELECT VARCHAR_FORMAT(CURRENT_TIMESTAMP, 'DDMMYYYYHHMM') AS reportingDate,
        al.CLOSED_DATE                                    AS closedDate,
        al.CERT_INCORPORATION                             AS certIncorporation,
        al.NATIONALITY                                    AS nationality,
-       al.AGENT_STATUS                                   AS agentStatus,
+       CASE
+           WHEN be.EMPL_STATUS = '1' THEN 'Active'
+           WHEN be.EMPL_STATUS = '0' THEN 'Inactive'
+           ELSE 'Suspended'
+           END                                           AS agentStatus,
        al.AGENT_TYPE                                     AS agentType,
        al.ACCOUNT_NUMBER                                 AS accountNumber,
        al.REGION                                         AS region,
@@ -24,4 +27,17 @@ SELECT VARCHAR_FORMAT(CURRENT_TIMESTAMP, 'DDMMYYYYHHMM') AS reportingDate,
        al.GPS_COORDINATES                                AS gpsCoordinates,
        al.AGENT_TAX_IDENTIFICATION_NUMBER                AS agentTaxIdentificationNumber,
        al.BUSINESS_LICENCE                               AS businessLicense
-FROM AGENTS_LIST_V4 al;
+FROM AGENTS_LIST_V4 al
+         JOIN BANKEMPLOYEE be
+              ON
+                  CASE
+                      WHEN LENGTH(REPLACE(al.TERMINAL_ID, ' ', '')) > 8
+                          THEN RIGHT(REPLACE(al.TERMINAL_ID, ' ', ''), 8)
+                      ELSE REPLACE(al.TERMINAL_ID, ' ', '')
+                      END
+                      =
+                  CASE
+                      WHEN LENGTH(REPLACE(be.STAFF_NO, ' ', '')) > 8
+                          THEN RIGHT(REPLACE(be.STAFF_NO, ' ', ''), 8)
+                      ELSE REPLACE(be.STAFF_NO, ' ', '')
+                      END
