2c5d97020aa8d2de0166b55e61fb13e49c799ad3
boniface mwandobo <bonifacesimon04@gmail.com>
2026-02-23 17:16:22 +0300

    introduction to fixing rates in the balance with other banks

--- sqls/balance-with-other-bank.sql
+++ sqls/balance-with-other-bank.sql
@@ -53,30 +53,5 @@ FROM GLI_TRX_EXTRACT as gte
 
          LEFT JOIN
      PROFITS.PROFITS_ACCOUNT pa ON gte.CUST_ID = pa.CUST_ID and PRFT_SYSTEM = 3
-
-         -- =========================================
--- Join Currency Using SHORT_DESCR
--- =========================================
-         LEFT JOIN CURRENCY curr
-                   ON curr.SHORT_DESCR = gte.CURRENCY_SHORT_DES
-
-    -- =========================================
--- Latest Fixing Rate Per Currency
--- =========================================
-         LEFT JOIN (SELECT fr.fk_currencyid_curr,
-                           fr.rate
-                    FROM fixing_rate fr
-                    WHERE (fr.fk_currencyid_curr, fr.activation_date, fr.activation_time) IN
-                          (SELECT fk_currencyid_curr,
-                                  activation_date,
-                                  MAX(activation_time)
-                           FROM fixing_rate
-                           WHERE activation_date = (SELECT MAX(b.activation_date)
-                                                    FROM fixing_rate b
-                                                    WHERE b.activation_date <= CURRENT_DATE)
-                           GROUP BY fk_currencyid_curr, activation_date)) fx
-                   ON fx.fk_currencyid_curr = curr.ID_CURRENCY
-
-
 where gl.EXTERNAL_GLACCOUNT IN ('100050001', '100013000', '100050000')
   AND pa.ACCOUNT_NUMBER <> '';
