bf87c53d8612904577c0ffc722bd51403b4fd764
boniface mwandobo <bonifacesimon04@gmail.com>
2026-02-24 16:47:17 +0300

    balance with mno till number

--- sqls/balances-with-mnos.sql
+++ sqls/balances-with-mnos.sql
@@ -1,39 +1,33 @@
-SELECT
-    VARCHAR_FORMAT(CURRENT_TIMESTAMP,'DDMMYYYYHHMM') AS reportingDate,
-    VARCHAR_FORMAT(CURRENT_TIMESTAMP,'DDMMYYYHHMM') AS floatBalanceDate,
+SELECT VARCHAR_FORMAT(CURRENT_TIMESTAMP, 'DDMMYYYYHHMM') AS reportingDate,
+       VARCHAR_FORMAT(CURRENT_TIMESTAMP, 'DDMMYYYHHMM')  AS floatBalanceDate,
+       CASE gl.EXTERNAL_GLACCOUNT
+           WHEN '144000058' THEN 'Tigo Pesa'
+           WHEN '144000062' THEN 'M-Pesa'
+           END                                           AS mnoCode,
+       CASE gl.EXTERNAL_GLACCOUNT
+           WHEN '144000058' THEN '0710-338790'
+           WHEN '144000062' THEN '711758'
+           END                                           AS tillNumber,
+       gte.CURRENCY_SHORT_DES                            AS currency,
+       0                                                 AS allowanceProbableLoss,
+       0                                                 AS botProvision,
+       gte.DC_AMOUNT                                     AS orgFloatAmount,
+       CASE
+           WHEN gte.CURRENCY_SHORT_DES = 'USD'
+               THEN DECIMAL(gte.DC_AMOUNT, 18, 2)
 
-    -- mnoCode based on EXTERNAL_GLACCOUNT
-    CASE gl.EXTERNAL_GLACCOUNT
-        WHEN '504080001' THEN 'Jumo'
-        WHEN '144000051' THEN 'Airtel Money'
-        WHEN '144000058' THEN 'Tigo Pesa'
-        WHEN '144000061' THEN 'Halopesa'
-        WHEN '144000062' THEN 'M-Pesa'
-    END AS mnoCode,
+           WHEN gte.CURRENCY_SHORT_DES <> 'USD'
+               THEN DECIMAL(gte.DC_AMOUNT / fx.rate, 18, 2)
 
-    pa.ACCOUNT_NUMBER AS tillNumber,
-    gte.CURRENCY_SHORT_DES AS currency,
-    0 AS allowanceProbableLoss,
-    0 AS botProvision,
+           ELSE NULL
+           END                                           AS usdFloatAmount,
 
-    gte.DC_AMOUNT                                      AS orgFloatAmount,
+       CASE
+           WHEN gte.CURRENCY_SHORT_DES = 'USD'
+               THEN DECIMAL(gte.DC_AMOUNT * fx.rate, 18, 2)
 
-    CASE
-        WHEN gte.CURRENCY_SHORT_DES = 'USD'
-            THEN DECIMAL(gte.DC_AMOUNT, 18, 2)
-
-        WHEN gte.CURRENCY_SHORT_DES <> 'USD'
-            THEN DECIMAL(gte.DC_AMOUNT / fx.rate, 18, 2)
-
-        ELSE NULL
-        END                                            AS usdFloatAmount,
-
-    CASE
-        WHEN gte.CURRENCY_SHORT_DES = 'USD'
-            THEN DECIMAL(gte.DC_AMOUNT * fx.rate, 18, 2)
-
-        ELSE DECIMAL(gte.DC_AMOUNT, 18, 2)
-        END                                            AS tzsFloatAmount
+           ELSE DECIMAL(gte.DC_AMOUNT, 18, 2)
+           END                                           AS tzsFloatAmount
 
 FROM GLI_TRX_EXTRACT gte
          -- Join Currency Using SHORT_DESCR
@@ -59,8 +53,6 @@ FROM GLI_TRX_EXTRACT gte
                    ON fx.fk_currencyid_curr = curr.ID_CURRENCY
 
          JOIN GLG_ACCOUNT gl
-    ON gte.FK_GLG_ACCOUNTACCO = gl.ACCOUNT_ID
-LEFT JOIN PROFITS_ACCOUNT pa ON pa.CUST_ID = gte.CUST_ID
-WHERE gl.EXTERNAL_GLACCOUNT IN (
-    '504080001','144000051','144000058','144000061','144000062'
-);
+              ON gte.FK_GLG_ACCOUNTACCO = gl.ACCOUNT_ID
+         LEFT JOIN PROFITS_ACCOUNT pa ON pa.CUST_ID = gte.CUST_ID
+WHERE gl.EXTERNAL_GLACCOUNT IN ('144000058', '144000062');
