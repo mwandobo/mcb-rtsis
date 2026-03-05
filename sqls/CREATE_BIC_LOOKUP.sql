ae573d902501b1cc565d66b6e757b3ccfabcadf2
boniface mwandobo <bonifacesimon04@gmail.com>
2026-03-05 22:13:19 +0300

    outgoing refactor

--- sqls/CREATE_BIC_LOOKUP.sql
+++ sqls/CREATE_BIC_LOOKUP.sql
@@ -2,5 +2,5 @@
 create table BANK_BIC_LOOKUP
 (
     BANK_NAME   VARCHAR(200)  null,
-    BIC   VARCHAR(50)  null,
+    BIC   VARCHAR(50)  null
 );
--- sqls/outgoing-fund-transfer-v1.sql
+++ sqls/outgoing-fund-transfer-v1.sql
@@ -1,10 +1,10 @@
-select VARCHAR_FORMAT(CURRENT_TIMESTAMP, 'DDMMYYYYHHMM')                                     AS reportingDate,
-       out.REFERENCE_NUMBER                                                                  AS transactionId,
-       VARCHAR_FORMAT(out.TRX_DATE, 'DDMMYYYYHHMM')                                          AS transactionDate,
-       'EFT'                                                                                 AS transferChannel,
-       NULL                                                                                  AS subCategoryTransferChannel,
-       out.ISSUER_NAME                                                                       AS senderName,
-       out.ORD_CUST_ACCOUNT                                                                  AS senderAccountNumber,
+select VARCHAR_FORMAT(CURRENT_TIMESTAMP, 'DDMMYYYYHHMM')                                      AS reportingDate,
+       out.REFERENCE_NUMBER                                                                   AS transactionId,
+       VARCHAR_FORMAT(out.TRX_DATE, 'DDMMYYYYHHMM')                                           AS transactionDate,
+       'EFT'                                                                                  AS transferChannel,
+       NULL                                                                                   AS subCategoryTransferChannel,
+       out.ISSUER_NAME                                                                        AS senderName,
+       out.ORD_CUST_ACCOUNT                                                                   AS senderAccountNumber,
        CASE
            -- National ID (NIDA) : 20 numeric digits
            WHEN LENGTH(out.ISSUER_ID_NUM) = 20
@@ -16,36 +16,68 @@ select VARCHAR_FORMAT(CURRENT_TIMESTAMP, 'DDMMYYYYHHMM')                        
            -- Taxpayer Identification Number (TIN) : 11 digits
            WHEN LENGTH(out.ISSUER_ID_NUM) = 11
                AND out.ISSUER_ID_NUM NOT LIKE '%[^0-9]%'
-               THEN 'TaxIdentificationNumber'
+               THEN 'Certificate of Registration'
            -- Driving Licence (common TZ pattern)
            WHEN out.ISSUER_ID_NUM LIKE '400%'
                THEN 'DrivingLicence'
-           ELSE 'Other'
-           END                                                                               AS senderIdentificationType,
-       out.ISSUER_ID_NUM                                                                     AS senderIdentificationNumber,
-       out.BENEF_NAME                                                                        AS recipientName,
-       out.BENEF_PHONE                                                                       AS recipientMobileNumber,
-       CASE UPPER(TRIM(out.BENEF_COUNTRY)) WHEN 'TZ' THEN 'TANZANIA, UNITED REPUBLIC OF' END AS recipientCountry,
-       out.ACC_WITH_BANK_SWIF                                                                AS recipientBankOrFspCode,
-       out.BENEF_ACCOUNT                                                                     AS recipientAccountOrWalletNumber,
-       'Mobile banking'                                                                      AS serviceChannel,
-       'Mobile banking'                                                                      AS serviceCategory,
-       'Inter banking'                                                                       AS serviceSubCategory,
-       out.ORDER_PAYABLE_CUR                                                                 AS currency,
-       out.ORDER_AMOUNT                                                                      AS orgAmount,
+           ELSE null
+           END                                                                                AS senderIdentificationType,
+       out.ISSUER_ID_NUM                                                                      AS senderIdentificationNumber,
+       out.BENEF_NAME                                                                         AS recipientName,
+       out.BENEF_PHONE                                                                        AS recipientMobileNumber,
+       CASE UPPER(TRIM(out.BENEF_COUNTRY)) WHEN 'TZ' THEN 'TANZANIA, UNITED REPUBLIC OF' END  AS recipientCountry,
+       CASE UPPER(TRIM(out.ACC_WITH_BANK_SWIF))
+           WHEN 'KCBLTZTZXXX' THEN 'KCBLTZTZ'
+           WHEN 'ECOCTZTZXXX' THEN 'ECOCTZTZ'
+           WHEN 'UCCTTZTZXXX' THEN 'UCCTTZTZF'
+           WHEN 'UNAFTZTZXXX' THEN 'UNAFTZTZ'
+           WHEN 'ADVBTZTZAXX' THEN 'ADVBTZTZ'
+           WHEN 'BOFAUS3DAU2' THEN 'BOFAUS6SXXX'
+           WHEN 'NBIMGB21XXX' THEN 'NEIMGB2LXXX'
+           WHEN 'CORUTZT10T2' THEN 'CORUTZTZ'
+           WHEN 'EQBLTZTZXXX' THEN 'EQBLTZTZ'
+           WHEN 'BNKMTZTZXXX' THEN 'MELIDE21XXX'
+           WHEN 'TARATZTZXXX' THEN 'TANZTZTXXXX'
+           WHEN 'DASUTZTZXXX' THEN 'DASUTZTZ'
+           WHEN 'NLCBTZTX0T3' THEN 'NLCBTZTX'
+           WHEN 'HABLTZTZXXX' THEN 'HABLTZTZ'
+           WHEN 'SFICTZTZXXX' THEN 'SFIPUS31XXX'
+           WHEN 'DASUTZTZXXXX' THEN 'DASUTZTZ'
+           WHEN 'PBZATZTZXXX' THEN 'PBZATZTZ'
+           WHEN 'FNMITZTZXXX' THEN 'FNMITZTZ'
+           WHEN 'AKCOTZTZXXX' THEN 'AKCOTZTZ'
+           WHEN 'KLMJTZTZXXX' THEN 'KLMJTZTZ'
+           WHEN 'FMBZTZTXXXX' THEN 'FMBZTZTX'
+           WHEN 'AZANTZTZXXX' THEN 'AZANTZTZ'
+           WHEN 'TAPBTZTZXXX' THEN 'TAPBTZTZ'
+           WHEN 'NOSCINBBXXX' THEN 'NOSCIE2XXXX'
+           WHEN 'NLCBTZTXTAN' THEN 'NLCBTZTX'
+           WHEN 'MKCBTZTZXXX' THEN 'MKCBTZTZ'
+           WHEN 'HSBCHKHHHKH' THEN 'HSBCHKHHXXX'
+           WHEN 'TAINTZTZXXX' THEN 'BKMYTZTZ'
+           WHEN 'FIRNTZTXXXX' THEN 'EXTNTZTZF'
+           WHEN 'NLCBTZTXZAN' THEN 'NLCBTZTX'
+           ELSE out.ACC_WITH_BANK_SWIF
+           END                                                                                AS recipientBankOrFspCode,
+       out.BENEF_ACCOUNT                                                                      AS recipientAccountOrWalletNumber,
+       'Mobile banking'                                                                       AS serviceChannel,
+       'Mobile banking'                                                                       AS serviceCategory,
+       'Inter banking'                                                                        AS serviceSubCategory,
+       out.ORDER_PAYABLE_CUR                                                                  AS currency,
+       out.ORDER_AMOUNT                                                                       AS orgAmount,
        CASE
            WHEN out.ORDER_PAYABLE_CUR = 'USD' THEN out.ORDER_AMOUNT
            WHEN out.ORDER_PAYABLE_CUR = 'TZS' THEN 0
            WHEN out.ORDER_PAYABLE_CUR <> 'USD' THEN DECIMAL(out.ORDER_AMOUNT / fx.RATE, 18, 2)
            ELSE NULL
-           END                                                                               AS usdAmount,
+           END                                                                                AS usdAmount,
        CASE
            WHEN out.ORDER_PAYABLE_CUR = 'USD'
                THEN DECIMAL(out.ORDER_AMOUNT * fx.rate, 18, 2)
            ELSE DECIMAL(out.ORDER_AMOUNT, 18, 2)
-           END                                                                               AS tzsAmount,
-       'Salaries and wages'                                                                  AS purposes,
-       out.SPECIAL_TERMS                                                                     AS senderInstruction,
+           END                                                                                AS tzsAmount,
+       'Salaries and wages'                                                                   AS purposes,
+       COALESCE(out.SPECIAL_TERMS, 'MAINTENANCE EXPENSES FOR FAMILY')                         AS senderInstruction,
        CASE UPPER(TRIM(out.ISSUER_COUNTRY)) WHEN 'TZ' THEN 'TANZANIA, UNITED REPUBLIC OF' END AS transactionPlace
 from OUTGOING_ORDERS out
          LEFT JOIN (SELECT fr.fk_currencyid_curr,
